ddfdfaacee8d81c3e771e4f590b1e13634767f79
Rocco Sicilia <rocco.sicilia@protonmail.com>
2024-02-20 20:24:20 +0100

    edir struct

--- NetScan/ArpInfo.py
+++ NetScan/ArpInfo.py
@@ -17,22 +17,14 @@ else:
     adapter = sys.argv[2]
 
 # arp scan
-def arp_scan(target_ip, interface):
+arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=1, pdst=target_list)
+ans,uans = srp(arp_request, timeout=2, iface=adapter, verbose=False)
 
-    # packet
-    arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=1, pdst=target_ip)
-    answer, _ = srp(arp_request, timeout=2, iface=interface, verbose=True)
-
-    # JSON output
-    result = []
-    for sent, received in answer:
+result = []
+for sent, received in ans:
         result.append({'IP': received.psrc, 'MAC': received.hwsrc})
-    
-    return result
 
-# main script
-
-for target in target_list:
-    scan_result = arp_scan(target, adapter)
-    for entry in scan_result:
-        print("IP: {}, MAC: {}".format(entry['IP'], entry['MAC']))
+# arp scan output
+for entry in result:
+        print(entry)
+        
