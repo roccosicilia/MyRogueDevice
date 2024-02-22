ddef87bc0d59aff0fc7937023227e7060245a6cc
Rocco Sicilia <rocco.sicilia@protonmail.com>
2024-02-22 17:38:22 +0100

    back

--- NetScan/ArpInfo.py
+++ NetScan/ArpInfo.py
@@ -11,16 +11,16 @@ if len(sys.argv) >= 3:
     target_list = sys.argv[1]
     adapter = sys.argv[2]
 else:
-    target_list = ['192.168.0.0/24', '172.16.0.0/20']
+    target_list = '192.168.0.0/24'
     adapter = 'eth1' # change if different
 
 # arp scan
+arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=1, pdst=target_list)
+ans,uans = srp(arp_request, timeout=2, iface=adapter, verbose=False)
+
 result = []
-for target in target_list:
-    arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=1, pdst=target)
-    ans,uans = srp(arp_request, timeout=2, iface=adapter, verbose=False)
-    for sent, received in ans:
-        result.append({'IP': received.psrc, 'MAC': received.hwsrc})
+for sent, received in ans:
+    result.append({'IP': received.psrc, 'MAC': received.hwsrc})
 
 # arp scan output
 for entry in result:
