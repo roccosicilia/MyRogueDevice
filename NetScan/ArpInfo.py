8d3dfc575645eb4852218c51dd4b5a569dc0e6a9
Rocco Sicilia <rocco.sicilia@protonmail.com>
2024-02-22 17:47:40 +0100

    add sniffer

--- NetScan/ArpInfo.py
+++ NetScan/ArpInfo.py
@@ -18,6 +18,8 @@ else:
 arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=1, pdst=target_list)
 ans,uans = srp(arp_request, timeout=2, iface=adapter, verbose=False)
 
+print("+---------------------------------------------+")
+print("IP list from arp-scan")
 result = []
 for sent, received in ans:
     result.append({'IP': received.psrc, 'MAC': received.hwsrc})
@@ -26,4 +28,15 @@ for sent, received in ans:
 for entry in result:
     print(entry)
 
-# arp sniff
+# arp sniff func
+sniffer_timeout = 10
+captured_pkt = []
+def arp_sniff(packet):
+    if ARP in packet and packet[ARP].op == 1:
+        captured_pkt.append(packet)
+
+sniff(filter='arp', prn=arp_sniff, timeout=sniffer_timeout)
+
+print("+---------------------------------------------+")
+print("Sniffed ARP packets: {}".format(len(captured_pkt)))
+print(captured_pkt)
